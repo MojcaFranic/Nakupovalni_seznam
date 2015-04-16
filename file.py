e28b5265b592a466175a44c4056d338e2fdff377
Mojca Franic <mojcafranic1@gmail.com>
2015-04-16 13:12:14 +0200

    nakupovalni seznam

---
+++ file.py
@@ -0,0 +1,27 @@
+nakupovalni_seznam = []
+
+print("Naredi nakupovalni seznam: ")
+
+prvi_artikel = raw_input("Kaj zelis kupiti? ")
+
+odgovor = "da"
+
+while odgovor == "da":
+    odgovor = raw_input("Ali zelis kaj dodati? da/ne ")
+
+    if odgovor == "ne":
+        print("Tvoj seznam: ")
+        for artikel in nakupovalni_seznam:
+            print("- ") +  artikel
+        break
+    elif odgovor == "da":
+
+
+
+        dodatek = raw_input("Povej kaj zelis dodati? ")
+
+    nakupovalni_seznam.append(dodatek)
+
+
+
+
